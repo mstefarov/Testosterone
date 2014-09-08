f779d8bd7dd4302f22b9f5157193c5fb8fce951a
fragmer <me@matvei.org>
2014-09-08 13:13:31 -0600

    Added IPacket interface. OOPey netcode is necessary for logging/feature flexibility.

---
+++ Testosterone/Packets/IPacket.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Testosterone.Packets {
+    internal interface IPacket {
+        OpCode OpCode { get; }
+        byte[] Data { get; }
+    }
+}
